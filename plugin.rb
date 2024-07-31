24e4803a9bb5ece5f8871d557d787ea0782b4180
joo2017 <30431754+joo2017@users.noreply.github.com>
2024-07-31 17:55:13 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -6,34 +6,32 @@
 
 enabled_site_setting :video_player_enabled
 
-register_asset 'javascripts/discourse/initializers/init-video-player.js.es6'
+register_asset "stylesheets/video-js.css"
+register_asset "javascripts/discourse/initializers/init-video-player.js.es6"
 
 after_initialize do
-  require_dependency 'onebox/engine/video_onebox'
-  
-  class ::Onebox::Engine::VideoOnebox
-    matches_regexp(%r{^(https?:)?//.*\.(mov|mp4|webm|ogv|m3u8)(\?.*)?$}i)
+  require_dependency "onebox"
 
-    def to_html
-      if @url.match(%r{\.m3u8$})
-        randomId = Time.now.to_i.to_s + rand(100000000).to_s
-        <<-HTML
-          <div class="onebox video-onebox videoWrap">
-            <video id='#{randomId}' class="video-js vjs-default-skin vjs-16-9" controls preload="auto" width="100%" data-setup='{"fluid": true}'>
-              <source src="#{@url}" type="application/x-mpegURL">
-            </video>
-          </div>
-        HTML
-      else
-        escaped_url = ::Onebox::Helpers.normalize_url_for_output(@url)
-        <<-HTML
-          <div class="onebox video-onebox">
-            <video width='100%' height='100%' controls #{@options[:disable_media_download_controls] ? 'controlslist="nodownload"' : ""}>
-              <source src='#{escaped_url}'>
-              <a href='#{escaped_url}'>#{@url}</a>
-            </video>
-          </div>
-        HTML
+  module ::Onebox
+    class Engine
+      class VideoOnebox
+        include Engine
+
+        matches_regexp(%r{^(https?:)?//.*\.(mov|mp4|webm|ogv|m3u8)(\?.*)?$}i)
+
+        def to_html
+          if @url.match(%r{\.m3u8$})
+            <<-HTML
+              <div class="onebox video-onebox">
+                <video class="video-js" controls preload="auto" width="100%" data-setup='{"fluid": true}'>
+                  <source src="#{@url}" type="application/x-mpegURL">
+                </video>
+              </div>
+            HTML
+          else
+            super
+          end
+        end
       end
     end
   end
