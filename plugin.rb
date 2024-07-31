1557b6a1fabed07743fe4709378dfa53d78ad214
joo2017 <30431754+joo2017@users.noreply.github.com>
2024-07-31 19:20:56 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,22 +1,25 @@
-after_initialize do
-  require_dependency "onebox"
+# name: discourse-video-player
+# about: Adds support for m3u8 video files using Video.js player
+# version: 0.1
+# authors: Your Name
+# url: https://github.com/yourusername/discourse-video-player
 
-  module ::Onebox
-    module Engine
-      class VideoOnebox
-        def to_html
-          if @url.match(%r{\.m3u8$})
-            <<-HTML
-              <div class="onebox video-onebox">
-                <video class="video-js" controls preload="auto" width="100%" data-setup='{"fluid": true}'>
-                  <source src="#{@url}" type="application/x-mpegURL">
-                </video>
-              </div>
-            HTML
-          else
-            super
-          end
-        end
+enabled_site_setting :video_player_enabled
+
+register_asset "stylesheets/videojs-discourse.scss"
+register_asset "javascripts/discourse/initializers/init-video-player.js.es6"
+
+after_initialize do
+  require_dependency "onebox/engine/video_onebox"
+
+  class ::Onebox::Engine::VideoOnebox
+    alias_method :old_to_html, :to_html
+    
+    def to_html
+      if @url.match?(/\.m3u8(\?.*)?$/)
+        "<div class='onebox video-onebox'><video class='video-js' controls preload='auto' width='100%' data-setup='{\"fluid\": true}'><source src='#{@url}' type='application/x-mpegURL'></video></div>"
+      else
+        old_to_html
       end
     end
   end
