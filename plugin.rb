a159befde1afc32942ddc85b7a8a3bb906e39afd
joo2017 <30431754+joo2017@users.noreply.github.com>
2024-07-31 18:49:35 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,24 +1,9 @@
-# name: discourse-video-player
-# about: Adds support for m3u8 video files using Video.js player
-# version: 0.1
-# authors: Your Name
-# url: https://github.com/yourusername/discourse-video-player
-
-enabled_site_setting :video_player_enabled
-
-register_asset "stylesheets/video-js.css"
-register_asset "javascripts/discourse/initializers/init-video-player.js.es6"
-
 after_initialize do
   require_dependency "onebox"
 
   module ::Onebox
-    class Engine
+    module Engine
       class VideoOnebox
-        include Engine
-
-        matches_regexp(%r{^(https?:)?//.*\.(mov|mp4|webm|ogv|m3u8)(\?.*)?$}i)
-
         def to_html
           if @url.match(%r{\.m3u8$})
             <<-HTML
